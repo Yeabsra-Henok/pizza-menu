f8b433903df96375c60fd01a3b03da6fb70d9025
Yeabsra Henok <75983453+Yeabsra-Henok@users.noreply.github.com>
2024-05-24 16:38:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ function Header() {
 
   return (
     <header className="header">
-      <h1 style={style}>Fast React Pizza Co.</h1>
+      <h1 style={style}>Pizza Italiano</h1>
     </header>
   );
 }
